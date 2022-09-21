13c1a981e6c27d6d8235feb20956197dd0fdcf07
Frederico Leles Santos <fred.leles@hotmail.com>
2022-09-21 13:13:30 -0300

    Updates the MainView and MainVM to support navigation through a inner component

--- DesktopUI/ViewModels/MainPageViewModel.cs
+++ DesktopUI/ViewModels/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DesktopUI.ViewModels
 {
@@ -12,10 +13,18 @@ namespace DesktopUI.ViewModels
         public string UserName { get; set; }
         private ILoggedInUserModel _loggedInUser;
 
+        public ICommand NavigateToPDV { get; }
+        public ICommand NavigateToProducts { get; }
+        public ICommand NavigateToSales { get; }
+        public ICommand NavigateToUsers { get; }
+
+        public ViewModelBase CurrentViewModel { get; }
+
         public MainPageViewModel(ILoggedInUserModel loggedInUser)
         {
             _loggedInUser = loggedInUser;
             UserName = _loggedInUser.EmailAddress;
+            CurrentViewModel = new PdvViewModel();
         }
     }
 }
