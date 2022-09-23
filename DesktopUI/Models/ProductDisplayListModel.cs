a2e927a295c8e74fd7c9efa03e9c685d514268dc
Frederico Leles Santos <fred.leles@hotmail.com>
2022-09-22 21:46:02 -0300

    Changes the layout of the Products View

---
+++ DesktopUI/Models/ProductDisplayListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Models
+{
+    internal class ProductDisplayListModel
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Barcode { get; set; } = string.Empty;
+        public decimal RetailPrice { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
--- DesktopUI/ViewModels/ProductsViewModel.cs
+++ DesktopUI/ViewModels/ProductsViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ApiDataAccess.Library.Models;
+using DesktopUI.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,36 @@ namespace DesktopUI.ViewModels
 {
     internal class ProductsViewModel : ViewModelBase
     {
+        public ProductsViewModel()
+        {
+            _products.Add(new ProductDisplayListModel()
+            {
+                Id = 1,
+                ProductName = "Produto1",
+                Barcode = "123445",
+                QuantityInStock = 10,
+                RetailPrice = 9.90m,
+            });
+            _products.Add(new ProductDisplayListModel()
+            {
+                Id = 2,
+                ProductName = "Produto2",
+                Barcode = "123445",
+                QuantityInStock = 1,
+                RetailPrice = 99.90m,
+            });
+        }
+
+        private BindingList<ProductDisplayListModel> _products = new();
+        public BindingList<ProductDisplayListModel> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                OnPropertyChanged(nameof(Products));
+            }
+        }
+
     }
 }
