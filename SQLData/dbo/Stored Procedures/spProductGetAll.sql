b84f3e8ab1d4558b847d7006b49af84f7e160abb
Frederico Leles Santos <fred.leles@hotmail.com>
2022-09-24 12:55:21 -0300

    Feat: Creates the products table and the stored procedure to get all products

---
+++ SQLData/dbo/Stored Procedures/spProductGetAll.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spProductGetAll]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT Id, ProductName, Barcode, [Description], RetailPrice, QuantityInStock, ProductImage
+	FROM dbo.Product
+	ORDER BY ProductName;
+END
---
+++ SQLData/dbo/Tables/Product.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Barcode] NVARCHAR(100), 
+    [Description] NVARCHAR(MAX), 
+    [RetailPrice] MONEY NOT NULL, 
+    [QuantityInStock] INT NOT NULL DEFAULT 1,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(),
+    [ProductImage] NVARCHAR(500) NULL
+)
